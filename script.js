040336e9240a9761ff81198467fa32e6259c8624
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-08-10 13:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let count = 0; // initial value
+
+    const countDisplay = document.getElementById("count");
+    const incrementBtn = document.getElementById("incrementBtn");
+
+    incrementBtn.addEventListener("click", function() {
+      count++;
+      countDisplay.textContent = count;
+    });
